6499db38754088e8fbf391a5e86cca4c6e5f9f39
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-07-11 20:10:40 -0500

    Login GUI hecho

--- src/Bibliografia.java
+++ src/Bibliografia.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class Bibliografia {
+public class Bibliografia {
 }
--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Login {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login {
+    public JPanel login;
+    public JTextArea titulo;
+    public JTextField textuser;
+    public JPasswordField Textpassword;
+    public JButton btnsesion;
+    public JLabel usuario;
+    public JLabel contrasenia;
+
+    public Login() {
+        textuser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        Textpassword.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnsesion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String user = textuser.getText();
+                String pass = Textpassword.getText();
+
+                if(user.equals("richard") && pass.equals("123456")){
+                    JOptionPane.showMessageDialog(null, "Credenciales correctas");
+                }else{
+                    JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrecta. Intente de nuevo!.");
+                }
+            }
+        });
+    }
 }
